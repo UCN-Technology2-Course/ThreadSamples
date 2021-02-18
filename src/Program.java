b14008685cd7d738742ca435510ec51ef62174ee
Lars Nysom <LANY@ucn.dk>
2021-02-18 12:11:18 +0100

    Added Thread extension

--- src/Program.java
+++ src/Program.java
@@ -7,7 +7,15 @@ public class Program {
 	    System.out.println("Hello world!\n");
 
 	    // Running threads
-	    
+	    GreetingsThread t1 = new GreetingsThread("T1");
+	    GreetingsThread t2 = new GreetingsThread("T2");
+	    GreetingsThread t3 = new GreetingsThread("T3");
+	    GreetingsThread t4 = new GreetingsThread("T4");
+
+	    t1.start();
+	    t2.start();
+	    t3.start();
+	    t4.start();
 	    
 	    
 	    System.out.println("threads started, goodbye cruel world \n");
