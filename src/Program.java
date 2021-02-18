5bee3c984e7aa00b001d4b795bc670ba7580ef52
Lars Nysom <LANY@ucn.dk>
2021-02-18 11:50:13 +0100

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,15 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		//create your threads here, eg. with new
+	    System.out.println("Hello world!\n");
+
+	    //run all your threads here!
+	    System.out.println("threads stared, goodbye cruel world \n");
+	    
+	    
+	}
+
+}
