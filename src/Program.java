8fd6ab12cfd4c105cfb50c82fafe390da0a203a7
Lars Nysom <LANY@ucn.dk>
2021-02-19 14:35:40 +0100

    Changed Program

--- src/Program.java
+++ src/Program.java
@@ -4,48 +4,49 @@ public class Program {
 	public static void main(String[] args) throws InterruptedException {
 
 		// Starting program
-	    System.out.println("Hello world!");
+		System.out.println("Hello world!");
 
-	    // Creating threads
-	    
-	    // Thread extension
-	    GreetingsThread t1 = new GreetingsThread("T1");
-	    GreetingsThread t2 = new GreetingsThread("T2");
-	    GreetingsThread t3 = new GreetingsThread("T3");
-	    GreetingsThread t4 = new GreetingsThread("T4");   
-	    
-	    // Runnable implementation
-	    GreetingsRunnable r1 = new GreetingsRunnable("R1");
-	    GreetingsRunnable r2 = new GreetingsRunnable("R2");
-	    GreetingsRunnable r3 = new GreetingsRunnable("R3");
-	    GreetingsRunnable r4 = new GreetingsRunnable("R4");
+		// Creating threads
 
-	    Thread tr1 = new Thread(r1);
-	    Thread tr2 = new Thread(r2);
-	    Thread tr3 = new Thread(r3);
-	    Thread tr4 = new Thread(r4);
-	    
-	    // Starting threads
-	    t1.start();
-	    t2.start();
-	    t3.start();
-	    t4.start();	 	
-	    tr1.start();
-	    tr2.start();
-	    tr3.start();
-	    tr4.start();	
-	    
-	    t1.join();
-	    t2.join();
-	    t3.join();
-	    t4.join();
-	    tr1.join();
-	    tr2.join();
-	    tr3.join();
-	    tr4.join();	    
-	    
-	    System.out.println("threads started, goodbye cruel world");
-	    // Main thread finished
+		// Thread extension
+//	    GreetingsThread t1 = new GreetingsThread("T1");
+//	    GreetingsThread t2 = new GreetingsThread("T2");
+//	    GreetingsThread t3 = new GreetingsThread("T3");
+//	    GreetingsThread t4 = new GreetingsThread("T4");   
+
+		// Runnable implementation
+		GreetingsRunnable r1 = new GreetingsRunnable("R1");
+		GreetingsRunnable r2 = new GreetingsRunnable("R2");
+		GreetingsRunnable r3 = new GreetingsRunnable("R3");
+		GreetingsRunnable r4 = new GreetingsRunnable("R4");
+
+		Thread tr1 = new Thread(r1);
+		Thread tr2 = new Thread(r2);
+		Thread tr3 = new Thread(r3);
+		Thread tr4 = new Thread(r4);
+
+		// Starting threads
+//	    t1.start();
+//	    t2.start();
+//	    t3.start();
+//	    t4.start();	
+
+//	    t1.join();
+//	    t2.join();
+//	    t3.join();
+//	    t4.join();
+
+		tr1.start();
+		tr1.join();
+		tr2.start();
+		tr2.join();
+		tr3.start();
+		tr3.join();
+		tr4.start();
+		tr4.join();
+
+		System.out.println("threads started, goodbye cruel world");
+		// Main thread finished
 	}
 
 }
